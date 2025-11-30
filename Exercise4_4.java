4cb7d558bc615fd6276c8b41b38eae2a69f61e66
Ali-Istanbullu <250542019@firat.edu.tr>
2025-11-30 15:13:43 +0300

    Update Exercise4_4.java
    gorev 4.4 tamamlandi

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,11 @@
+/*
+ * Ad Soyad: [ALİ İSTANBULLU]
+ * Ogrenci No: [250542019]
+ * Tarih: [30.11.2025]
+ * Aciklama:4.4 tamamlandı
+ *
+ *
+ */
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -11,10 +19,10 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
-        getNumber();
+        getNumber();                                         // boş doner bir şey yazdırmaz
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
-        // System.out.println(sayHello() + 7);
+        // System.out.println(sayHello() + 7);                    //hata verir kırmızı yanar
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
