d4f32951f02d3c82ff39e1bbdbb38d8c100bdff6
Ali-Istanbullu <250542019@firat.edu.tr>
2025-12-04 14:41:25 +0300

    Update Exercise4_6_Multadd.java
    gorev 4.6 tamamlandı

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,16 +1,37 @@
-public class Exercise4_6_Multadd {
+/*
+ * Ad Soyad: [ALİ İSTANBULLU]
+ * Ogrenci No: [250542019]
+ * Tarih: [4.12.2025]
+ * Aciklama:4.6 tamamlandı
+ *
+ *
+ */
 
-    public static double multadd(double a, double b, double c) {
-        // TODO
-        return 0.0;
+public class Multadd {
+    static double expSum(double x){
+
+        return x*(Math.exp(-x))+Math.sqrt(1-Math.exp(-x));
+
     }
 
-    public static double expSum(double x) {
-        // TODO
-        return 0.0;
+    static double multadd(double a , double b , double c){
+
+       
+       return a*b+c;
+       
+
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        
+      double islem1 = Math.sin(Math.toRadians(45)) + Math.cos(Math.toRadians(45)) / 2 ;
+
+       double islem2 = Math.log10(10)+Math.log10(20);
+
+       System.out.printf("%.3f\n",islem2);
+       System.out.printf("%.3f\n",islem1);
+       System.out.printf("%.2f\n",multadd(1.0, 2.0, 3.0));
+        System.out.printf("%.2f\n",expSum(5));
     }
+    
 }
